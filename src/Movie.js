58348e91b4bd28849c81a739fc7260fab107ffaa
CHSDD <100271148+CHSDD@users.noreply.github.com>
2022-12-26 01:36:20 +0900

    done

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,30 @@
-import React from "react"
-import propTypes from "prop-types"
+import React from "react";
+import propTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
-    return (
-      <div>
-        <h5>{title}</h5>
-        <span>{year}</span>
-      </div>
-    );
+function Component(e) {
+  if ( e.length > 280 ) {
+    return <span>{e.slice(0, 280)}...</span>;
+  } else {
+    return <span>{e}</span>;
   }
+} 
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie_list">
+      <div className="movie_poster">
+        <img src={poster} alt={title} />
+      </div>
+      <div className="movie_contents">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="movie_genres">{genres.map((genres, index) => <li key={index} className="genres_list"> {genres} </li>)}</ul>
+        <p className="movie_summary">{Component(summary)}</p>
+      </div>
+    </div>
+  );
+}
 
 Movie.propTypes = {
   id: propTypes.number.isRequired,
@@ -16,6 +32,7 @@ Movie.propTypes = {
   title: propTypes.string.isRequired,
   summary: propTypes.string.isRequired,
   poster: propTypes.string.isRequired,
-}
+  genres: propTypes.array.isRequired
+};
 
 export default Movie;
